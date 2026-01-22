02b60ed734d33484b7207f8382dcecf1a1eabaee
GabrielRiart <gabogonzalez.py@gmail.com>
2026-01-22 17:52:23 -0300

    Solucione lo que exploto no se ni pq caraio

--- springprueba/src/main/java/com/pruebantrapida/app1/springprueba/SpringpruebaApplication.java
+++ springprueba/src/main/java/com/pruebantrapida/app1/springprueba/SpringpruebaApplication.java
@@ -2,7 +2,6 @@ package com.pruebantrapida.app1.springprueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringpruebaApplication {
